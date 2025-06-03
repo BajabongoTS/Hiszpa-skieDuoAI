61aebd4dc49e85c94d42c4d93eb6a6ea4d529113
Konrad Klautzsch <konradklautzsch@gmail.com>
2025-06-03 20:16:17 +0200

    szkielet aplikacji

---
+++ hiszpanskiDuoAI/src/components/navbar.tsx
@@ -0,0 +1,18 @@
+import "../styles/navbar.css"
+
+function NavBar() {
+    return (
+        <>
+            <nav>
+                <h2>DíaLingua</h2>
+                <div className="navigatrion">
+                    <p>ChatAI</p>
+                    <p>Lessons</p>
+                    <p>Kalendary</p>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default NavBar
---
+++ hiszpanskiDuoAI/src/views/MainPage.tsx
@@ -0,0 +1,13 @@
+import NavBar from "../components/navbar.tsx";
+
+function MainPage() {
+    return (
+        <>
+            <NavBar/>
+
+            <h1>Helloo</h1>
+        </>
+    )
+}
+
+export default MainPage
